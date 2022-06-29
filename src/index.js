5796e5981fe6b20585df728148c8df52e8c3c799
luoting.01 <luoting.01@bytedance.com>
2022-06-29 10:38:05 +0800

    commit

--- src/index.js
+++ src/index.js
@@ -3,20 +3,67 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 class Square extends React.Component {
+    /**
+     * 构造函数
+     */
+    /*
+    constructor(props) {
+        super(props);
+        this.state = {
+            value : null,
+        };
+    }
+    
+
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button 
+            className="square" 
+            onClick={() => {this.setState({value: "X"}); }} 
+        >
+            {this.state.value}
+        
         </button>
       );
     }
+    */
+    render () {
+        return (
+            /* button是内在组件，它的onClick属性是固定的，所以名字不能随便命名，但是Square组件是自定义组件，属性名字可以随便起，如此处叫onSquareClick
+            注意：此处容易写成onClick={() => this.props.onSquareClick}> 如果属性值是一个函数，则必须要带上（）
+            */
+            <button className='square' onClick={() => this.props.onSquareClick()}>
+                {this.props.value}
+            </button>
+        )
+    }
+
   }
   
   class Board extends React.Component {
-    renderSquare(i) {
-      return <Square />;
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares : Array(9).fill(null),
+        };
     }
-  
+
+    handleSquareClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = "X";
+        this.setState({squares : squares});
+    }
+
+    renderSquare(i) {
+      return (
+        <Square 
+            value={this.state.squares[i]}
+            /**属性值随便命名 */
+            onSquareClick = {() => this.handleSquareClick(i)}
+        />
+      );
+    }
+
     render() {
       const status = 'Next player: X';
   
